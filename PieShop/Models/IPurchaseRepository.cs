c0f184209f6255c4cbf7062e3e904bedac238af0
Dieseled-UP <l00099023@student.lyit.ie>
2018-06-14 14:27:01 +0100

    Added the IPurchaseRepo that all user to retrieve all purchases, or one by id, or one by customer id

---
+++ PieShop/Models/IPurchaseRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PieShop.Models
+{
+    public interface IPurchaseRepository
+    {
+        IEnumerable<Purchase> GetAllPurchases();
+        Purchase GetPurchaseById(int id);
+        Purchase GetPurchaseByCustomerId(int id);
+    }
+}

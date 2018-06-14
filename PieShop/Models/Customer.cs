00be2d5a9b432fd53a3d2d97b5f1794f8c6a9fa4
Dieseled-UP <l00099023@student.lyit.ie>
2018-06-14 10:51:24 +0100

    Added new Customer POCO and an ICustomerRepo

---
+++ PieShop/Models/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PieShop.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string AddressLine1 { get; set; }
+        public string Town { get; set; }
+        public string County { get; set; }
+        public string PostCode { get; set; }
+    }
+}

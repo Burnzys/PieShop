510644d0cdd4b9b031cec04046cc6604038b8b7b
Dieseled-UP <l00099023@student.lyit.ie>
2018-06-14 01:30:50 +0100

    Made IPieRepo public and instantiated it in the HomeController

--- PieShop/Controllers/HomeController.cs
+++ PieShop/Controllers/HomeController.cs
@@ -10,6 +10,13 @@ namespace PieShop.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IPieRepository _pieRepository;
+
+        public HomeController(IPieRepository pieRepository)
+        {
+            _pieRepository = pieRepository;
+        }
+
         public IActionResult Index()
         {
             return View();

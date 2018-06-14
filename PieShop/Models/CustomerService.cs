0f01739473c692a4bdb13c06bc6d8c96e16730b0
Dieseled-UP <l00099023@student.lyit.ie>
2018-06-14 11:10:41 +0100

    Added CustomerService with seeded data

---
+++ PieShop/Models/CustomerService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PieShop.Models
+{
+    public class CustomerService: ICustomerRepository
+    {
+
+        private List<Customer> _customers;
+
+        public CustomerService()
+        {
+            InitializeCustomers();
+        }
+
+        private void InitializeCustomers()
+        {
+            _customers = new List<Customer>
+            {
+                new Customer{Id = 1, Name = "James Dunne", AddressLine1 = "94 Ballytrack", Town = "Letterkenny", County = "Donegal", PostCode = "JT56 F43"},
+                new Customer{Id = 1, Name = "Susan Murray", AddressLine1 = "5 Main Street", Town = "Sligo", County = "Sligo", PostCode = "GY6 TR5"},
+                new Customer{Id = 1, Name = "John Doe", AddressLine1 = "Hilly", Town = "Ballyhunas", County = "Louth", PostCode = "BS34 UT9"}
+            };
+        }
+
+        public IEnumerable<Customer> GetAllCustomers()
+        {
+            return _customers;
+        }
+
+        public Customer GetCustomerById(int Id)
+        {
+            return _customers.FirstOrDefault(c => c.Id == Id);
+        }
+    }
+
+
+}
